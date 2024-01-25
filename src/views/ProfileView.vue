ab52a896844fb6d9622373385a2849e0e7ba377d
eloygm92 <eloygm.92@uma.es>
2024-01-25 10:06:02 +0100

    Fix: login to vercel

---
+++ src/views/ProfileView.vue
@@ -0,0 +1,20 @@
+<template>
+  <el-row :gutter="24">
+    <el-col :offset="6" :span="12">
+      <UserForm :edit-data="String(userStore.user.id)" @reload="reload++" />
+    </el-col>
+  </el-row>
+</template>
+
+<script setup lang="ts">
+
+import UserForm from "@/components/UserForm.vue";
+import { useUserStore } from "@/stores/user";
+
+const userStore = useUserStore();
+
+</script>
+
+<style scoped>
+
+</style>
